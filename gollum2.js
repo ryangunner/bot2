1cfaaff5a4c5cd44579de9e73585bf1eae5e8233
ryangunner <117401889+ryangunner@users.noreply.github.com>
2023-05-06 19:17:52 +0300

    Add files via upload

---
+++ gollum2.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+const Web3 = require('web3');
+
+
+const bot = new TelegramBot('6030206630:AAEdsXx4s9_2gra6UhgFdHL02XZ6zNj1eDc', { polling: true });
+
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/02622daf652444d18a2f49d2f3ed24af'));
+
+async function getGasPrice() {
+  const gasPrice = await web3.eth.getGasPrice();
+  return gasPrice;
+}
+
+bot.onText(/\/gasfee/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  try {
+    const gasPrice = await getGasPrice();
+    const gasPriceInGwei = web3.utils.fromWei(gasPrice, 'gwei');
+    bot.sendMessage(chatId, `Current gas fee: ${gasPriceInGwei} Gwei`);
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(chatId, 'Error fetching gas fee. Please try again later.');
+  }
+});
+
